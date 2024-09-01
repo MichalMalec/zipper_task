666465902976451923a2267b3de89847d257c23a
michal <michalmalec5@o2.pl>
2024-09-01 16:57:27 +0200

    refact - sessions and documents controllers

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,28 +1,48 @@
 class Users::SessionsController < Devise::SessionsController
-    respond_to :json
+  respond_to :json
 
-    def create
-        user = User.find_for_database_authentication(email: params[:user][:email])
-        
-        if user&.valid_password?(params[:user][:password])
-          sign_in(user)
-          render json: { message: 'Logged in successfully.', jwt: current_token }, status: :ok
-        else
-          render json: { error: 'Invalid email or password' }, status: :unauthorized
-        end
-      end
+  def create
+    user = find_user
 
-    private
-  
-    def respond_with(resource, _opts = {})
-      render json: { message: 'Logged in successfully.', jwt: current_token }, status: :ok
+    if user&.valid_password?(password)
+      sign_in(user)
+      render_success
+    else
+      render_invalid_credentials
     end
+  end
 
-    def current_token
-        request.env['warden-jwt_auth.token']
-      end
-  
-    def respond_to_on_destroy
-      head :no_content
-    end
+  private
+
+  def find_user
+    User.find_for_database_authentication(email: email)
+  end
+
+  def email
+    params.dig(:user, :email)
+  end
+
+  def password
+    params.dig(:user, :password)
+  end
+
+  def render_success
+    render json: { message: 'Logged in successfully.', jwt: current_token }, status: :ok
+  end
+
+  def render_invalid_credentials
+    render json: { error: 'Invalid email or password' }, status: :unauthorized
+  end
+
+  def current_token
+    request.env['warden-jwt_auth.token']
+  end
+
+  def respond_with(_resource, _opts = {})
+    render_success
+  end
+
+  def respond_to_on_destroy
+    head :no_content
+  end
 end
--- app/controllers/v1/documents_controller.rb
+++ app/controllers/v1/documents_controller.rb
@@ -1,29 +1,57 @@
 class V1::DocumentsController < ApplicationController
-    def index
-        @documents = current_user.documents
-        render json: @documents, status: :ok
-    end
-    
-    def create
-      @document = current_user.documents.build(document_params)
-      
-      if @document.save
-        # Generate a random password
-        password = SecureRandom.hex(8)
-        
-        # Generate ZIP file with password protection
-        zip_file_path = ZipGenerator.generate_zip_with_password(@document, password)
-        download_link = "#{request.base_url}/downloads/#{File.basename(zip_file_path)}"
+  def index
+    render json: documents, status: :ok
+  end
   
-        render json: { title: @document.title, download_link: download_link, password: password }, status: :created
-      else
-        render json: { errors: @document.errors.full_messages }, status: :unprocessable_entity
-      end
+  def create
+    document = build_document
+
+    if document.save
+      handle_successful_save(document)
+    else
+      handle_failed_save(document)
     end
-    
-      private
-    
-      def document_params
-        params.require(:document).permit(:title, :file)
-      end
+  end
+  
+  private
+
+  def documents
+    current_user.documents
+  end
+
+  def build_document
+    current_user.documents.build(document_params)
+  end
+
+  def document_params
+    params.require(:document).permit(:title, :file)
+  end
+
+  def handle_successful_save(document)
+    password = generate_random_password
+    zip_file_path = ZipGenerator.generate_zip_with_password(document, password)
+    download_link = generate_download_link(zip_file_path)
+
+    render json: success_response(document, download_link, password), status: :created
+  end
+
+  def handle_failed_save(document)
+    render json: { errors: document.errors.full_messages }, status: :unprocessable_entity
+  end
+
+  def generate_random_password
+    SecureRandom.hex(8)
+  end
+
+  def generate_download_link(zip_file_path)
+    "#{request.base_url}/downloads/#{File.basename(zip_file_path)}"
+  end
+
+  def success_response(document, download_link, password)
+    {
+      title: document.title,
+      download_link: download_link,
+      password: password
+    }
+  end
 end
