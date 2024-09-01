4ec61710174d021cc7879fa062fcb16529c6f299
michal <michalmalec5@o2.pl>
2024-09-01 16:48:04 +0200

    feat - secure downloaded file with password

--- app/controllers/v1/documents_controller.rb
+++ app/controllers/v1/documents_controller.rb
@@ -5,17 +5,21 @@ class V1::DocumentsController < ApplicationController
     end
     
     def create
-        @document = current_user.documents.build(document_params)
-    
-        if @document.save
-          zip_file_path = ZipGenerator.generate_zip(@document)
-          download_link = "#{request.base_url}/downloads/#{File.basename(zip_file_path)}"
+      @document = current_user.documents.build(document_params)
+      
+      if @document.save
+        # Generate a random password
+        password = SecureRandom.hex(8)
+        
+        # Generate ZIP file with password protection
+        zip_file_path = ZipGenerator.generate_zip_with_password(@document, password)
+        download_link = "#{request.base_url}/downloads/#{File.basename(zip_file_path)}"
   
-          render json: { id: @document.id, title: @document.title, download_link: download_link }, status: :created
-        else
-          render json: { errors: @document.errors.full_messages }, status: :unprocessable_entity
-        end
+        render json: { title: @document.title, download_link: download_link, password: password }, status: :created
+      else
+        render json: { errors: @document.errors.full_messages }, status: :unprocessable_entity
       end
+    end
     
       private
     
--- app/services/zip_generator.rb
+++ app/services/zip_generator.rb
@@ -1,24 +1,47 @@
-require 'zip'
-
 class ZipGenerator
-    def self.generate_zip(document)
-      download_dir = Rails.root.join('public', 'downloads')
-      Dir.mkdir(download_dir) unless File.directory?(download_dir)
-
-      file_name = "#{document.title.parameterize}.zip"
-      file_path = Rails.root.join('public', 'downloads', file_name)
-
-      File.delete(file_path) if File.exist?(file_path)
-
-      Zip::File.open(file_path, Zip::File::CREATE) do |zipfile|
-        original_file = document.file.download
-        original_filename = document.file.filename.to_s
-
-        zipfile.get_output_stream(original_filename) do |f|
-        f.write(original_file)
-        end
-      end
-
+    DOWNLOAD_DIR = Rails.root.join('public', 'downloads')
+    TMP_DIR = Rails.root.join('tmp')
+  
+    def self.generate_zip_with_password(document, password)
+      ensure_directory_exists(DOWNLOAD_DIR)
+  
+      file_name = generate_zip_file_name(document.title)
+      file_path = DOWNLOAD_DIR.join(file_name)
+  
+      delete_if_exists(file_path)
+  
+      original_file_path = save_original_file_to_temp(document)
+  
+      create_encrypted_zip(file_path, original_file_path, password)
+  
+      delete_if_exists(original_file_path)
+  
       file_path
     end
-end
+  
+    private
+  
+    def self.ensure_directory_exists(dir)
+      Dir.mkdir(dir) unless File.directory?(dir)
+    end
+  
+    def self.generate_zip_file_name(title)
+      "#{title.parameterize}.zip"
+    end
+  
+    def self.delete_if_exists(file_path)
+      File.delete(file_path) if File.exist?(file_path)
+    end
+  
+    def self.save_original_file_to_temp(document)
+      file_path = TMP_DIR.join(document.file.filename.to_s)
+      File.open(file_path, 'wb') do |file|
+        file.write(document.file.download)
+      end
+      file_path
+    end
+  
+    def self.create_encrypted_zip(zip_file_path, original_file_path, password)
+      system("zip -j -P #{password} #{zip_file_path} #{original_file_path}")
+    end
+  end
